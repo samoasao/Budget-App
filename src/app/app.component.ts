c5d89b21536ea12aed5c185a72afc1ab61984e70
Tyler Wright <samoasao@gmail.com>
2017-06-05 22:19:14 -0700

    Added category class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Category } from './category';
+
+const CATEGORIES: Category[] = [
+	{id: 1, name:'Grocery', ammount: 180},
+	{id: 2, name:'Gas', ammount: 125},
+	{id: 3, name:'Mortgage', ammount: 1500},
+	{id: 4, name:'Utilites', ammount: 200},
+
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Budget App';
+  categories = CATEGORIES;
 }
